d26551474e1536f06eb1baea64b4fa5d38cbcfa7
rahul825 <60977199+rahul825@users.noreply.github.com>
2021-07-01 21:11:17 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React ,{ Component } from 'react';
+import Dash from './Components/Dash';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+class App extends Component{
+   
+  render(){
+    return (  
+            <div>
+            <Dash />
+            </div>
+
+      
+       
+     );
+  }
+}
+
+ export default App; 
+
+
+
